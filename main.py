647e6554875c6dd9028608a87e0dd2d7ad42d781
AkXbots <122038135+AkXbots@users.noreply.github.com>
2023-05-11 22:43:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ BOT_TOKEN = ""
 OWNER_ID = "6273496002"
 SUDO_ID = "5836096186,6242479501"
 COWNER_ID = "5416322777"
-OP  = [ int(OWNER_ID), int(SUDO_ID), int(COWNER_ID)]
 #TelegramClient..
 sree = TelegramClient(
     "BanAll",
